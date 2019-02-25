175308f4ac4be77586268946174827fff207ba2e
Sumanth Kumar <meesalasumanth1@gmail.com>
2019-02-25 15:56:46 +1100

    login and register added

--- app/src/main/java/au/com/emerg/taxitowncars/activites/SplashActivity.kt
+++ app/src/main/java/au/com/emerg/taxitowncars/activites/SplashActivity.kt
@@ -3,13 +3,12 @@ package au.com.emerg.taxitowncars.activites
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import au.com.emerg.taxitowncars.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
 }
